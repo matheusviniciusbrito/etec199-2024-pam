0751d394b3764e4e796f2e4a3b9c4f4b865f3520
matheusviniciusbrito <matheus.vinicius.mvrb@gmail.com>
2024-10-07 16:34:35 -0300

    Funcoes JavaScript

---
+++ Aula 3 - Funções/calcular.js
@@ -0,0 +1,22 @@
+function calcularSoma(num1, num2){
+    console.log(num1 + num2);
+}
+
+
+function calcularSubtracao(num1, num2){
+    console.log(num1 - num2);
+}
+
+function calcularMultiplicacao(num1, num2){
+    console.log(num1 * num2);
+}
+
+function calcularDivisao(num1, num2){
+    console.log(num1 / num2);
+}
+
+
+calcularSoma(5, 2);
+calcularSubtracao(5, 2);
+calcularMultiplicacao(5, 2);
+calcularDivisao(5, 2);
