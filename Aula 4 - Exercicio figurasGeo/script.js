8a89db653ed45304c6ed8677b0bd9cca470cf9f3
Matheus Vinicius <78880370+matheusviniciusbrito@users.noreply.github.com>
2024-10-11 07:50:00 -0300

    Inicio

---
+++ Aula 4 - Exercicio figurasGeo/script.js
@@ -0,0 +1,22 @@
+const form = document.getElementById('formulario');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const nLados = document.getElementById('inputLados').value;
+    if (nLados === NaN){
+        
+    } else {
+        
+    }
+
+
+
+    console.log(`Numero de lados: ${nLados}`)
+
+
+
+
+
+  });
+    
